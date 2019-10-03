09d17585f878dbad5838f8b167eca4477b8a3b9b
Charla Bigelow <33845593+ConspiracyTheory13@users.noreply.github.com>
2019-10-02 21:05:06 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Requirements:
 // The user must be able to search for a GitHub user handle.
 // The search must trigger a call to GitHub's API.
 // The repos associated with that handle must be displayed on the page.
@@ -7,44 +6,39 @@
 
 //GET /users/:username/repos
 
+const clientId= "Iv1.a4450a0e9f570615";
+const clientSecret="A92273779e1b33882c40c3fed8c0f2ff9777b5246";
+
 // First, on submit of my form userLookup the .userInput value is ran against all
 //repos on github.
 
-const key="";
-
 $(".userLookup").submit(function(event) {
     event.preventDefault();  
-    fetch('https://dog.ceo/api/breeds/image/random/'+$(".userInput").val())
+    fetch('https://api.github.com/users/'+$(".userLookup").val()+ 'repos')
       .then(response => response.json())
-      .then(responseJson => printDogImages(responseJson));
+      .then(responseJson => displayUserRepo(responseJson));
     }
 );
 
-function getDogByBreed(userInputBreed) {
-  //   console.log(userInputBreed);
-  //   if(userInputBreed.message === "Breed not found (master breed does not exist)") 
-  //   { displayError();
-  //   } else { 
-  //   empty();
-  //   let breedImg = '<li><img src="'+ userInputBreed.message + '"></li>';
-  //   $(".dogsByBreedContainer").append(breedImg);}
-  // };
-  
+//next the repoContainer is filled with the repo coinciding with user name
+const displayUserRepo = async (user) => {
+  console.log("displayUserFiring")
+  const apiCall = await fetch(`https://api.github.com/users/${user}?clientId=${clientId}&clientSecret=${clientSecret}`);
+  const data = await apiCall.json();
+  return {data};
+};
+
+const showData = () => {
+  displayUserRepo(userInput.value).then((result) => {
+  console.log("showDataFiring")
+  }
+)};
+ 
 function empty(){
     $( ".repoContainer" ).empty();
   };
 
 function displayError(){
   $(".repoContainer").html("<li>Sorry, git username is not found. Please try again.</li>");
+  showData();
 }
-// $(".bringTheDogBreed").submit(function(event) {
-//   event.preventDefault();  
-//   fetch('https://dog.ceo/api/breed/'+$(".userInputBreed").val().toLowerCase()+`/images/random`)
-//     .then(response => response.json())
-//     .then(responseJson => getDogByBreed(responseJson))
-//     .catch(function(error) 
-//       {console.log('There has been a problem with your fetch operation: ', error.message);
-//       displayError();
-//       });
-//     });
-
